6996b5016a57f97ddf3cc99c855bab569329bec8
Lloyd Lentz <cledwyn@gmail.com>
2018-06-13 15:27:13 -0500

    cleaning up transition to PDO

--- pyrologger.php
+++ pyrologger.php
@@ -4,19 +4,11 @@ include 'settings.php';
 $data = $_POST["data"];
 
 if($data){
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
-
     if(isset($_POST['coreid'])){
         $fld = $_POST['event'];
         $val = $_POST['data'];
         $coreid = $_POST['coreid'];
         //Log the data into MySQL
-        //$sql = "Insert into logger(field,val,coreid) values ('$fld','$val','$coreid')";
         //use prepared statement in PDO syntax to avoid sql injections
         $stmt = $dbh->prepare("INSERT INTO logger(field,val,coreid) values (:field, :value, :coreid)");
         $stmt->bindParam(':field', $fld);
