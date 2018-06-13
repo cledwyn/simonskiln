d86c6bdf6a8b38b8d07b92526ede7993f3abd9a4
Lloyd Lentz <cledwyn@gmail.com>
2018-06-13 11:03:07 -0500

    Add webhook endpoint used by particle.io

---
+++ pyrologger.php
@@ -0,0 +1,39 @@
+<?php
+include 'settings.php';
+
+$firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
+
+$data = $_POST["data"];
+
+if($data){
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    if(isset($_POST['coreid'])){
+        $fld = $_POST['event'];
+        $val = $_POST['data'];
+        $coreid = $_POST['coreid'];
+        //Log the data into MySQL
+        $sql = "Insert into logger(field,val,coreid) values ('$fld','$val','$coreid')";
+        if ($conn->query($sql) === TRUE) {
+            echo $sql;
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        //Log the data into Firebase
+        $dateTime = new DateTime();
+        $dateTimeZone = new DateTimeZone('America/Chicago');        
+        $dateTime->setTimeZone($dateTimeZone);
+        $fb = array('event' => $fld,
+            'data' => floatval($val) > 0 ? floatval($val) : $val,
+            'time'=> $dateTime->format('c')
+         );
+        $firebase->set(DEFAULT_PATH . '/' . $coreid . '/'. $dateTime->format('c'), $fb);
+    }
+    $conn->close();
+}
